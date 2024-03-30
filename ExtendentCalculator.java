d58e179c314c45300f9e0dea7fcff3328734da24
shaheen <01fe22bca103@kletech.ac.in>
2024-03-30 22:05:04 +0530

    code for extended calculator

---
+++ ExtendentCalculator.java
@@ -0,0 +1,11 @@
+
+    class ExtendedCalculator extends Calculator {
+        public float square(float num) {
+            return (num * num);
+        }
+     
+        public float cube(float num) {
+            return (num * num * num);
+        }
+     }
+
